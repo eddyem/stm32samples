1b6f652834f311b559605f08e8f22031e350f9e0
eddyem <eddyem@yandex.ru>
2015-03-21 22:48:29 +0300

    first run

---
+++ simple_cdc/mk/sync.d
@@ -0,0 +1,10 @@
+mk/sync.o: sync.c /usr/local/arm-none-eabi/include/libopencm3/cm3/sync.h \
+ /usr/local/arm-none-eabi/include/libopencm3/cm3/common.h \
+ /usr/lib/gcc/arm-none-eabi/4.7.3/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/_ansi.h \
+ /usr/lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/newlib.h \
+ /usr/lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/sys/config.h \
+ /usr/lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/machine/ieeefp.h \
+ /usr/lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/sys/features.h \
+ /usr/lib/gcc/arm-none-eabi/4.7.3/include/stdbool.h
