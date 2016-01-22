075ae65951f70d8e74623fa73c429ea3faadaa66
eddyem <eddyem@yandex.ru>
2016-01-22 14:24:22 +0300

    add FSMC imitation using timer or DMA

---
+++ DMA_GPIO/dmagpio.h
@@ -0,0 +1,32 @@
+/*
+ * dmagpio.h
+ *
+ * Copyright 2016 Edward V. Emelianov <eddy@sao.ru, edward.emelianoff@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ */
+#pragma once
+#ifndef __DMAGPIO_H__
+#define __DMAGPIO_H__
+
+#include "main.h"
+#include "hardware_ini.h"
+
+void dmagpio_init();
+void dmagpio_transfer(uint8_t *databuf, uint32_t length);
+extern int transfer_complete;
+
+#endif // __DMAGPIO_H__
